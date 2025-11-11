a2cbf5ca83b1233f552de2967e4e3925214db2e5
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-11-10 23:07:41 -0300

    backend para render.com

--- services/emailService.js
+++ services/emailService.js
@@ -1,24 +1,38 @@
-const sendContactEmail = (name, email, company, companyType, message) => {
 
+const nodemailer = require("nodemailer");
+
+// 1. A DEFINIÇÃO DA VARIÁVEL
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS  
+    }
+});
+
+// 2. CRIAÇÃO DA FUNÇÃO ESPECIALIZADA
+const sendContactEmail = (name, email, company, companyType, message) => {
+    
     const mailOptions = {
         from: process.env.EMAIL_USER,
         to: process.env.EMAIL_RECEIVER,
         replyTo: email,
-        subject: `Novo contato de ${name} (Empresa: ${company || 'N/A'})`,
+        subject: `Novo Contato de ${name} (Empresa: ${company || 'N/A'})`,
         html: `
-        <h2>Nova mensagem de contato do site: </h2>
-        <p><strong>Nome:</strong>${name}</p>
-        <p><strong>Email:</strong>${email}</p>
-        <p><strong>Empresa:</strong>${company || 'Não informado'}</p>
-        <p><strong>Tipo de Empresa</strong> ${companyType || 'Não informado'}
-        <hr>
-        <h3>Mensagem:</h3>
-        <p>${message.replace(/\n/g, '<br>')}</p>
+            <h2>Nova mensagem de contato do site:</h2>
+            <p><strong>Nome:</strong> ${name}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Empresa:</strong> ${company || 'Não informado'}</p>
+            <p><strong>Tipo de Empresa:</strong> ${companyType || 'Não informado'}</p>
+            <hr>
+            <h3>Mensagem:</h3>
+            <p>${message.replace(/\n/g, "<br>")}</p>
         `
     };
 
+    // 3. O USO DA VARIÁVEL
     return transporter.sendMail(mailOptions);
-}
+};
 
 module.exports = {
     sendContactEmail
