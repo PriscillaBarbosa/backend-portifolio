b8488b4ecc25ba4b69aa743dbdb03d3a1885ad35
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-11-12 23:43:46 -0300

    alterando a porta

--- services/emailService.js
+++ services/emailService.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 // 1. A DEFINIÇÃO DA VARIÁVEL
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false, 
+    port: 465,
+    secure: true, 
     auth: {
         user: process.env.BREVO_USER, 
         pass: process.env.BREVO_PASS  
