5768e2d765669babde3e343fdc250254b1e420df
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-11-12 23:04:38 -0300

    ajustando o e-mail service

--- services/emailService.js
+++ services/emailService.js
@@ -3,10 +3,12 @@ const nodemailer = require("nodemailer");
 
 // 1. A DEFINIÇÃO DA VARIÁVEL
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false, 
     auth: {
-        user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS  
+        user: process.env.BREVO_USER, 
+        pass: process.env.BREVO_PASS  
     }
 });
 
