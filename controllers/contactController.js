f1d7bf9abf5e7a8843d2e557b5856e74f42a54f0
Priscilla Barbosa <priscillabarbosa@outlook.com>
2025-11-17 16:48:10 -0300

    Corrige 'import' para 'require' no contactController

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,4 +1,5 @@
-import { sendContactEmail } from '../services/emailService';
+
+const { sendContactEmail } = require('../services/emailService');
 
 const handleContactForm = async (req, res) => {
     const { name, email, company, companyType, message } = req.body;
@@ -9,18 +10,14 @@ const handleContactForm = async (req, res) => {
 
     try {
         await sendContactEmail(name, email, company, companyType, message);
-
         res.status(200).json({ message: 'Mensagem enviada com sucesso.' });
 
     } catch (error) {
-
         console.error('Erro no controlador ao enviar e-mail:', error);
-
         res.status(500).json({ message: 'Falha ao enviar e-mail. Tente novamente.'})
     }
 };
 
-export default {
+module.exports = {
     handleContactForm
 }
-
